f0d51a6f8ae908dca8721a22d5bf9f8c2de40fb7
Dan Ott <dan@dtott.com>
2020-07-24 10:17:13 -0400

    does not happen with react router 6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 let Home = () => (
   <div>
     <h1>Home</h1>
     <nav>
       <Link to="/">Home</Link> |{' '}
-      <Link to="/dashboard" className="dashlink">
+      <Link to="dashboard" className="dashlink">
         Dashboard
       </Link>
     </nav>
@@ -19,14 +19,10 @@ let Dash = () => <div className="dashboard">Dash</div>;
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route path="/dashboard">
-          <Dash />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="dashboard" element={<Dash />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
